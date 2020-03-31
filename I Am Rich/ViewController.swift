c8994a11c7fe9d60d97349a12b4621c3fd196ce0
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-31 12:24:24 -0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Wesley Jacobs on 3/31/20.
+//  Copyright © 2020 Wesley Jacobs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
